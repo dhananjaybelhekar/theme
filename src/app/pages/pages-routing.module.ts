f51c42aba31f2a01bfd69e3d1a0f94686953a5eb
dhananjay belhekar <dbel431@gmail.com>
2018-02-27 22:32:53 +0530

    nested component

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PagesComponent} from './pages.component'
-const routes: Routes = [{ path: '', component: PagesComponent }];
+const routes: Routes = [{ path: '', component: PagesComponent,
+children:[
+  { path: 'show', loadChildren: './show/show.module#ShowModule'}
+] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/show/show.module.ts
+++ src/app/pages/show/show.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShowComponent } from './show.component';
-
+import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [{
+	path: '',
+	data: {
+      title: 'Accordion',
+      urls: [{title: 'Dashboard', url: '/'},{title: 'ngComponent'},{title: 'Accordion'}]
+    },
+	component: ShowComponent
+}];
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [ShowComponent]
 })
